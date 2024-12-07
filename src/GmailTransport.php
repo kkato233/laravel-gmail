dc9ad56e26ed33ceaac6e29958fd52b63e6e57c4
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2024-12-07 09:51:15 +0900

    LaravelGmail 実装を修正

--- src/GmailTransport.php
+++ src/GmailTransport.php
@@ -15,14 +15,41 @@ class GmailTransport extends AbstractTransport
 
     public function __construct(array $config)
     {
-        $this->config = $config;
+        $this->config = $config['mailers']['gmail'];
+
+        $from_address = $this->config['from_address'];
+        $service_account_key = $this->config['service_account_key'];
+        
+        $SCOPES = implode(' ', [Gmail::GMAIL_SEND]);
+        $APPLICATION_NAME = 'LaravelGmail';
 
         $this->client = new Client();
-        $this->client->setApplicationName(config('app.name'));
-        $this->client->setAuthConfig(base_path($this->config['service_account_key']));
-        $this->client->setScopes([Gmail::GMAIL_SEND]);
-        $this->client->setSubject($this->config['from_address']);
-
+        $this->client->setApplicationName($APPLICATION_NAME);
+        $this->client->setScopes($SCOPES);
+        $this->client->setAuthConfig($service_account_key);
+        $this->client->setAccessType('offline');
+        
+        // ファイルが無い場合にブラウザから認証を行う
+        if (!file_exists($service_account_key)) {
+            $authUrl = $this->client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+            $accessToken = $this->client->fetchAccessTokenWithAuthCode($authCode);
+            if (!file_exists(dirname($service_account_key))) {
+                mkdir(dirname($service_account_key), 0700, true);
+            }
+            file_put_contents($service_account_key, json_encode($accessToken));
+            printf("Credentials saved to %s\n", $service_account_key);
+        }
+        $accessToken = json_decode(file_get_contents($service_account_key), true);
+        $this->client->setAccessToken($accessToken);
+        // リフレッシュトークンを使ってアクセストークンを更新する
+        if ($this->client->isAccessTokenExpired()) {
+            $this->client->fetchAccessTokenWithRefreshToken($this->client->getRefreshToken());
+            file_put_contents($credentialsPath, json_encode($this->client->getAccessToken()));
+        }
+        
         parent::__construct();
     }
 
@@ -30,10 +57,14 @@ class GmailTransport extends AbstractTransport
     {
         $service = new Gmail($this->client);
 
-        $gmail_message = new Message();
-        $gmail_message->setRaw(base64_encode($message->getOriginalMessage()->toString()));
+        $rawMessageString = $message->getOriginalMessage()->toString();
 
-        $service->users_messages->send($this->config['from_address'], $gmail_message);
+        $rawMessage = base64_encode($rawMessageString);
+        $rawMessage = str_replace(['+', '/', '='], ['-', '_', ''], $rawMessage);
+        
+        $gmail_message = new Message();
+        $gmail_message->setRaw($rawMessage);
+        $results = $service->users_messages->send("me", $gmail_message);
     }
 
     public function __toString(): string
